9a3a1002c5b7e8608ad7120fa219db2f9543dca4
Wolfgang Huang <huang@ba00000-lex.srv.mwn.de>
2026-02-11 09:54:18 +0100

    Configure for reverse proxy deployment

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="pdl_ltlabtools_dictconsistency",
+    backend_host="0.0.0.0",
+    frontend_path="/dictconsistency",
+    # api_url="/dictconsistency",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
